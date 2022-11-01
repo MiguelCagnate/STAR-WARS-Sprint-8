62ba17a8587da25990b9f3f3515acac8c6659cd1
omikeflies <omikeflies@gmail.com>
2022-11-01 22:24:04 +0100

    issues con el login & welcome page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React,{Fragment, useState} from "react";
 import "./App.css";
 import logo from "./img/sw_logo.png";
 import { StarshipsRender } from "./Starships";
-import {FullDataShips} from './FullDataShips'
-import { Home } from "./Home";
+import { FullDataShips } from "./FullDataShips";
 import { Route, Routes, BrowserRouter, Link } from "react-router-dom";
+import { Home } from "./Home";
+
+
 
 
 
 export function App() {
 
+  const [isDetailVisible, toggleDetail] = useState(false);
   return (
-    
+
+  
     <BrowserRouter>
+      <div>
+      {isDetailVisible ? (
 
 
-<div>
-        <img src={logo} alt="star wars white logo" className="LogoStyle" />
-       
+
+<img src={logo} alt="star wars white logo" className="LogoStyle" />
+
         <Link to="/Home" className="link">
           <span></span>
           <span></span>
@@ -30,28 +36,44 @@ export function App() {
           <span></span>
           <span></span>
           <span></span>
-          Starships          
+          Starships
         </Link>
-<form>
-
-      <Link className="link">LOGIN</Link>
-      <Link className="link">SIGN UP</Link>
-      <Link className="link"  type="submit">Register</Link>
-
-      <p className="ParagraphLogin">Already Registered{" "} / Login in?</p>
-</form>
- 
-        <hr></hr>
-         <Routes>
-          <Route exact path="/Home" element={<Home />} />
-          <Route path="/StarshipsRender" element={<StarshipsRender />} />        
-          <Route path="/StarshipsRender/:id" element={<FullDataShips />} />        
-        </Routes>
       
-      </div>
+        <form>
+          <Link className="link">LOGIN </Link>
+          <Link  className="link">
+            SIGN UP
+          </Link>
+          <Link className="link" type="submit">
+            Register
+          </Link>
+
+          <p className="ParagraphLogin">Already Registered / Login in?</p>
+         
+        </form>
+        <hr></hr>
+        <Routes>
+          <Route exact path="/Home" element={<Home />} />
+          <Route path="/StarshipsRender" element={<StarshipsRender />} />
+          <Route path="/StarshipsRender/:id" element={<FullDataShips />} />
+        </Routes>
+
+        ) : (
+        <BackgroundPage>         
+          <h1>Benvinguts</h1>
+          <h3>Aquesta és la història d'un superheroi galàctic 🦹‍♀️</h3>
+          <ButtonStart type="button" onClick={() => toggleDetail(true)}>
+            START!▶️
+          </ButtonStart>
+        </BackgroundPage>
+      )}
+
 
 
      
+
+      </div>
     </BrowserRouter>
+   
   );
 }
--- src/Registered.js
+++ src/Registered.js
@@ -1,10 +1,103 @@
-// import React from "react";
+// import React,{useState} from "react";
+// import "./App.css";
+// import logo from "./img/sw_logo.png";
+// import { StarshipsRender } from "./Starships";
+// import { FullDataShips } from "./FullDataShips";
+// import { Home } from "./Home";
+// import { Route, Routes, BrowserRouter, Link } from "react-router-dom";
+// import { Login } from "./Login";
 
 
-// function Registered(){
-//     const [email, setEmail] = useState(""); 
-//     const [name, setName] = useState(""); 
-//     const [password, setPassword] = useState(""); 
-//     const [login, setLogin] = useState(true);
-    
-//   }
+// function Registered() {
+//   const [email, setEmail] = useState("");
+//   const [name, setName] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [phonenumber, setPhonenumber] = useState("");
+//   const [login, setLogin] = useState(true);
+//   const [flag, setFlag] = useState(false)
+// }
+
+// function handleSubmit(e){
+//   e.preventDefault();
+// if(!email || !name || !password || !phonenumber ) {
+//   setFlag(true);
+// }else{
+//   setFlag(False)
+//   localStorage.setItem("Email", JSON.stringify(email));
+//   localStorage.setItem("Password", JSON.stringify(password));
+// console.log("Save in local Storage");
+// setLogin(!Login)
+
+// }
+// }
+// function handleClick(params) {
+//   setLogin(!login)
+// }
+
+
+
+// export function App() {
+//   return (
+//     <BrowserRouter>
+//       <div>
+// {Login ? (
+
+// <>
+
+
+
+//         <img src={logo} alt="star wars white logo" className="LogoStyle" />
+//         <Link to="/Home" className="link">
+//           <span></span>
+//           <span></span>
+//           <span></span>
+//           <span></span>
+//           Home
+//         </Link>
+//         <Link to="/StarshipsRender" className="link">
+//           <span></span>
+//           <span></span>
+//           <span></span>
+//           <span></span>
+//           Starships
+//         </Link>
+      
+//         <form onSubmit={handlesubmit}>
+//           <Link
+//             className="link"
+//             onChange={(event) => setLogin(event.target.value)}
+//           >
+//             LOGIN
+//           </Link>
+//           <Link
+//             className="link"
+//             onChange={(event) => setName(event.target.value)}
+//           >
+//             SIGN UP
+//           </Link>
+//           <Link className="link" type="submit">
+//             Register
+//           </Link>
+
+//           <p className="ParagraphLogin" onClick={handleClick}>Already Registered / Login in?</p>
+
+//           {flag &&(
+//             <alert>
+//               Please fill every field
+//             </alert>
+//           )}
+
+//         </form>
+//         <hr></hr>
+//         <Routes>
+//           <Route exact path="/Home" element={<Home />} />
+//           <Route path="/StarshipsRender" element={<StarshipsRender />} />
+//           <Route path="/StarshipsRender/:id" element={<FullDataShips />} />
+//         </Routes>
+//   ):(
+//             <Login/>
+//         )}
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
